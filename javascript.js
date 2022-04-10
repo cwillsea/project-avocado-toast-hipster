232021c5a056d6c95af06c6730b4cb58c94b7337
Christian Willsea <christianwillsea@gmail.com>
2022-04-09 21:31:01 -0700

    Add pseudocode detailing plan and steps for game

---
+++ javascript.js
@@ -0,0 +1,5 @@
+//Create a script that plays rock paper scissors.
+//User will select avocado, toast, or hipster.
+//Computer will select avocado, toast, or hipster.
+//Script will compare the two values.
+//Script will return winner, loser, or tie.
