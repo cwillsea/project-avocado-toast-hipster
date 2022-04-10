6c537bf348af2467c617971cb77cb306a6e6c177
Christian Willsea <christianwillsea@gmail.com>
2022-04-09 22:04:58 -0700

    Create var for options & function for computer
    The computerPlay function will select a random option
    from the predefined options array. This will also call
    on the function in the script.

--- javascript.js
+++ javascript.js
@@ -4,5 +4,12 @@ let playerSelection;
 let computerSelection;
 //User will select avocado, toast, or hipster.
 //Computer will select avocado, toast, or hipster.
+const options = ['Avocado', 'Toast', 'Hipster'];
+computerPlay(options);
+function computerPlay(array){
+    const randomIndex = Math.floor(Math.random() * array.length);
+    const item = array[randomIndex];
+    return item;
+}
 //Script will compare the two values.
 //Script will return winner, loser, or tie.
