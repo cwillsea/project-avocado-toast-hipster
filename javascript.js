e8d87b9b10e339c5d80cc2fa6fd331f81ae017ed
Christian Willsea <christianwillsea@gmail.com>
2022-04-09 21:39:52 -0700

    Adjust user & computer input name
    This is per the instructions indicated in the odin project.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //Create a script that plays rock paper scissors.
 //Variables for user input and computer input.
-let userInput;
-let computerInput;
+let playerSelection;
+let computerSelection;
 //User will select avocado, toast, or hipster.
 //Computer will select avocado, toast, or hipster.
 //Script will compare the two values.
