41bfa589bd1ecac8385ad533e8ac8f67ea8676cd
Christian Willsea <christianwillsea@gmail.com>
2022-04-09 21:34:26 -0700

    Create variables for user and computer input

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 //Create a script that plays rock paper scissors.
+//Variables for user input and computer input.
+let userInput;
+let computerInput;
 //User will select avocado, toast, or hipster.
 //Computer will select avocado, toast, or hipster.
 //Script will compare the two values.
