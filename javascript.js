c59f62d31a977d9889e032617c85005cf0843d41
Christian Willsea <christianwillsea@gmail.com>
2022-04-10 17:31:31 -0700

    Prompt user for input, return round of play

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 //Create a script that plays rock paper scissors.
 //Variables for user input and computer input.
-let playerSelection;
+
+let playerSelection = window.prompt('Which do you decide: Avocado, Toast, or Hipster?', 'avocado');
+
+
 let computerSelection;
 let playerSelectionUpperCase = playerSelection.toUpperCase();
+console.log (`Player picked ${playerSelection}.`)
 //User will select avocado, toast, or hipster.
 //Computer will select avocado, toast, or hipster.
 const options = ['AVOCADO', 'TOAST', 'HIPSTER'];
@@ -15,9 +19,10 @@ function computerPlay(array){
     return item;
 }
 
-console.log(computerSelection);
+console.log(`Computer picked ${computerSelection}.`);
 
 //Script will compare the two values.
+console.log (playRound (computerSelection, playerSelection);
 //Script will return winner, loser, or tie.
 
 function playRound (computerSelection, playerSelection) {
@@ -36,3 +41,5 @@ function playRound (computerSelection, playerSelection) {
         (`You Win! ${computerSelection} eats the ${playerSelection}, but has a gluten allergy.`)
     }
 }
+
+//Game function to play multiple rounds.
