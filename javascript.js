5967b01c26d64f37034a1e249370bc9bf639bacc
Christian Willsea <christianwillsea@gmail.com>
2022-04-13 18:53:10 -0700

    Consolidate variables in roundPlay & game
    The variables for user input and computer input are in roundPlay now
    to make it easier on the game function. The game function plays 5 rounds
    and console logs the results and score of each round.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,11 @@
 //Create a script that plays rock paper scissors.
 //Variables for user input and computer input.
 
-let playerSelection = window.prompt('Which do you decide: Avocado, Toast, or Hipster?', 'avocado');
 
-
-let computerSelection;
-let playerSelectionUpperCase = playerSelection.toUpperCase();
-console.log (`Player picked ${playerSelectionUpperCase}.`)
 //User will select avocado, toast, or hipster.
 //Computer will select avocado, toast, or hipster.
 const options = ['AVOCADO', 'TOAST', 'HIPSTER'];
 
-computerSelection = computerPlay(options)
 
 function computerPlay(array){
     const randomIndex = Math.floor(Math.random() * array.length);
@@ -19,29 +13,61 @@ function computerPlay(array){
     return item;
 }
 
-console.log(`Computer picked ${computerSelection}.`);
+
 
 //Script will compare the two values.
-console.log (playRound (computerSelection, playerSelectionUpperCase));
 //Script will return winner, loser, or tie.
 
-function playRound (computer, player) {
 
-    if (computer === player) {
+function playRound () {
+
+    let playerSelection = window.prompt('Which do you decide: Avocado, Toast, or Hipster?', 'AVOCADO').toUpperCase();
+    let computerSelection = computerPlay(options);
+
+    console.log (`Player picked ${playerSelection}.`);
+    console.log(`Computer picked ${computerSelection}.`);
+
+
+    if (computerSelection === playerSelection) {
         return (`You Tie! You both picked ${playerSelection}.`);
-    } else if (computer === options[0] && player === options[1]) {
-        return (`You Lose! ${computer} spreads over ${player}.`); 
-    } else if (computer === options[1] && player === options[0]) {
-        return (`You Win! ${player} spreads over ${computer}.`);
-    } else if (computer === options[2] && player === options[0]){
-        return (`You Lose! ${computer} eats the ${player}.`);
-    } else if (computer === options[0] && player === options[2]){
-        return (`You Win! ${player} eats the ${computer}.`);
-    } else if (computer === options[1] && player === options[2]) {
-       return (`You Lose! ${player} eats the ${computer}, but has a gluten allergy.`);
-    } else if (computer === options[2] && player === options[1]) {
-        return (`You Win! ${computer} eats the ${player}, but has a gluten allergy.`)
+    } else if (computerSelection === options[0] && playerSelection === options[1]) {
+        return (`You Lose! ${computerSelection} spreads over ${playerSelection}.`); 
+    } else if (computerSelection === options[1] && playerSelection === options[0]) {
+        return (`You Win! ${playerSelection} spreads over ${computerSelection}.`);
+    } else if (computerSelection === options[2] && playerSelection === options[0]){
+        return (`You Lose! ${computerSelection} eats the ${playerSelection}.`);
+    } else if (computerSelection === options[0] && playerSelection === options[2]){
+        return (`You Win! ${playerSelection} eats the ${computerSelection}.`);
+    } else if (computerSelection === options[1] && playerSelection === options[2]) {
+       return (`You Lose! ${playerSelection} eats the ${computerSelection}, but has a gluten allergy.`);
+    } else if (computerSelection === options[2] && playerSelection === options[1]) {
+        return (`You Win! ${computerSelection} eats the ${playerSelection}, but has a gluten allergy.`)
     } return "That wasn't one of the options... do you even want to play?"
 }
 
+
 //Game function to play multiple rounds.
+
+
+function game() {
+
+    let computerScore = 0 ;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+      if (playRound().includes("You Win!")){
+          playerScore++;
+          console.log('Your score is ' + playerScore);
+          console.log('The computer score is ' + computerScore)
+      } else {
+          computerScore++;
+          console.log('Your score is ' + playerScore);
+          console.log('The computer score is ' + computerScore)
+      }
+        
+     }
+}
+
+game()
+
+
